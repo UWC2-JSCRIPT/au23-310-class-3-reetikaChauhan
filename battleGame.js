f336a31f449c59ac29bed58b7ad383ca49044c4c
Reetika <chauhanreetika45@gmail.com>
2023-10-26 18:05:54 -0700

    Solved battlegame,itemized and spaceship,not able to solve completely phone number

--- battleGame.js
+++ battleGame.js
@@ -2,14 +2,35 @@
 // attackingPlayer, defendingPlayer, baseDamage, variableDamage
 
 
+// const attack = function(attackingPlayer,defendingPlayer,baseDamage,variableDamage){
+//   let randomdamage = Math.floor(Math.random() * (variableDamage + 1))
+//   let totaldamage = baseDamage + randomdamage
+//   defendingPlayer.health -= totaldamage
+//   return (`${attackingPlayer.name} hits ${defendingPlayer.name} for ${totaldamage} damage`)
+
+// }
+
 
 // 2. Create player1 and player2 objects below
 // Each should have a name property of your choosing, and health property equal to 10
+const player1 = {
+  health: 10,
+  name : 'Merlin'
+}
+const player2 = {
+  health:10,
+  name:'Sophan'
+}
+console.log(attack(player1,player2,1,4))
 
+//3. Refactor attack function to an arrow function.  Comment out function above.
+const attack = (attackingPlayer,defendingPlayer,baseDamage,variableDamage) => {
+  let randomdamage = Math.floor(Math.random() * (variableDamage + 1))
+  let totaldamage = baseDamage + randomdamage
+  defendingPlayer.health -= totaldamage
+  return (`${attackingPlayer.name} hits ${defendingPlayer.name} for ${totaldamage} damage`)
 
-
-// 3. Refactor attack function to an arrow function.  Comment out function above.
-
+}
 
 
 // DO NOT MODIFY THE CODE BELOW THIS LINE
@@ -28,3 +49,24 @@ while (player1.health >= 1 && player2.health >= 1 && preventInfiniteLoop > 0) {
 }
 const eliminatedPlayer = player1.health <= 0 ? player1 : player2;
 console.log(`${eliminatedPlayer.name} has been eliminated!`);
+
+
+
+class SpaceShip {
+  constructor(name , topSpeed){
+    this.name = name
+    this.topSpeed = topSpeed
+  }
+
+  accelerate(){
+    const {name , topSpeed} = this
+    console.log(`${name} moving to ${topSpeed}`)
+  }
+}
+
+const ship1 = new SpaceShip('Nasa',5000 )
+const ship2 = new SpaceShip('SpaceX',6000 )
+const ship3 = new SpaceShip('ISRO',7000 )
+ship1.accelerate()
+ship2.accelerate()
+ship3.accelerate()
--- spaceShip.js
+++ spaceShip.js
@@ -3,7 +3,22 @@
 // - should have a method accelerate that logs to the console 
 //   `${name} moving to ${topSpeed}`
 
-
+class SpaceShip {
+    constructor(name , topSpeed){
+      this.name = name
+      this.topSpeed = topSpeed
+    }
+    accelerate(){
+        const {name , topSpeed} = this
+        console.log(`${name} moving to ${topSpeed}`)
+      }
+    }
 
 // 2. Call the constructor with a couple ships, 
 // and call accelerate on both of them.
+const ship1 = new SpaceShip('Nasa',5000 )
+const ship2 = new SpaceShip('SpaceX',6000 )
+const ship3 = new SpaceShip('ISRO',7000 )
+ship1.accelerate()
+ship2.accelerate()
+ship3.accelerate()
